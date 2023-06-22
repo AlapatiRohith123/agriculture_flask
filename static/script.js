d00c7d28e18003011933ac8f8400fadea4bba06d
AlapatiRohith123 <123347987+AlapatiRohith123@users.noreply.github.com>
2023-06-22 11:13:09 +0530

    2nd Commit

--- static/script.js
+++ static/script.js
@@ -1,8 +1,40 @@
-function fun(ph)
+function fun1(thiss)
 {
-    if(ph.value>14.0 || ph.value<0.0)
+    if(thiss.value>parseFloat(thiss.max) || thiss.value<parseFloat(thiss.min))
     {
-        alert("Please enter the correct Ph value. Ph value can only range from 0 to 14");
-        ph.value="";
+        alert("Please enter the correct Rainfall value. Rainfall value can only range from "+thiss.min+" to "+thiss.max);
+        thiss.value="";
+    }
+}
+function fun2(thiss)
+{
+    if(thiss.value>parseFloat(thiss.max) || thiss.value<parseFloat(thiss.min))
+    {
+        alert("Please enter the correct Minimum Temperature value. Minimum Temperature value can only range from "+thiss.min+" to "+thiss.max);
+        thiss.value="";
+    }
+}
+function fun3(thiss)
+{
+    if(thiss.value>parseFloat(thiss.max) || thiss.value<parseFloat(thiss.min))
+    {
+        alert("Please enter the correct Maximum Temperature value. Maximum Temperature value can only range from "+thiss.min+" to "+thiss.max);
+        thiss.value="";
+    }
+}
+function fun4(thiss)
+{
+    if(thiss.value>parseFloat(thiss.max) || thiss.value<parseFloat(thiss.min))
+    {
+        alert("Please enter the correct Irrigation value. Irrigation value can only range from "+thiss.min+" to "+thiss.max);
+        thiss.value="";
+    }
+}
+function fun5(thiss)
+{
+    if(thiss.value>parseFloat(thiss.max) || thiss.value<parseFloat(thiss.min))
+    {
+        alert("Please enter the correct Ph value. Ph value can only range from "+thiss.min+" to "+thiss.max);
+        thiss.value="";
     }
 }
